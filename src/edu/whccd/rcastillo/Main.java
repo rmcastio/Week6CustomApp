8c63c54b9294a113f703bcb738562ae3a62cd668
RichardCastillo <armed_deity@hotmail.com>
2020-05-04 18:12:53 -0700

    Pushing Commits. added corrections

--- src/edu/whccd/rcastillo/Main.java
+++ src/edu/whccd/rcastillo/Main.java
@@ -1,5 +1,7 @@
 package edu.whccd.rcastillo;
 
+import java.util.Scanner;
+
 /* Custom App 6 is to exhibit the use of Debugging and Testing.
     Author: Richard Castillo
  */
@@ -9,39 +11,35 @@ public class Main {
     public static void main(String[] args) {
 
         // 2. fix the runtime error caused by the line below by deleting, commenting, or encapsulating it in a try-catch block
-        getDouble(null, null);
+        //getDouble(null, null); Just commented out?
 
 
         // 3. fix the logic error so it calls the getDouble() on the 100th iteration
         for (int i = 1; i < 100; i++) {
-            if (i == 50)
+            if (i == 100) // was 50, changed to 100
             {
                 Scanner sc = new Scanner(System.in);
                 getDouble(sc, "Enter a double: ");
-            }
-            else
-            {
+            } else {
                 System.out.println(i);
             }
         }
     }
 
     // 1. fix the syntax errors in the method below
-    public static double getDouble(Scanner sc, String prompt)
-    {
-        d = 0.0;
-        while (true)
-        {
-            System.out.print(prompt)
+    public static double getDouble(Scanner sc, String prompt) {
+        double d = 0.0; //missing double
+        while (true) {
+            System.out.print(prompt); // missing SemiColon
 
-            if (sc.hasNextDouble() {
-            d = sc.NextDouble();
-            sc.nextLine();
-            return d;
-        }
-            else {
-            System.out.println("Error! Invalid number. Try again.");
-            sc.nextLine();
-        }
+            if (sc.hasNextDouble()) { //missing right bracket
+                d = sc.NextDouble();
+                sc.nextLine();
+                return d;
+            } else {
+                System.out.println("Error! Invalid number. Try again.");
+                sc.nextLine();
+            }
         }
     }
+}
